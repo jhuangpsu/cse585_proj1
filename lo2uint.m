b6862dfec196a327eed78d50b8b86fb8fe34eb82
CaptaiDrag <ipm5198@psu.edu>
2023-01-29 15:06:49 -0500

    add comment

--- lo2uint.m
+++ lo2uint.m
@@ -1,3 +1,27 @@
+% ==============================================================
+% Module: lo2uint.m
+%
+% Usage: Helper function
+%
+% Purpose:
+%   Doing a lo2uint operation.
+%
+% Input Variables:
+%   X The image to process with
+%
+% Returned Results:
+%   trans An image after transformation
+%
+% Processing Flow:
+%   1. Get size for X
+%   2. Transform all pixels
+%   3. Write output image
+%
+%
+% Author: Junzhe Huang, Yiyang Mei, Tianhui Li
+% Date: 01/29/2023
+% ===============================================================*
+
 function [trans] = lo2uint(X)
 trans = uint8(X.Neighborhood);
 [M,N] = size(trans);
